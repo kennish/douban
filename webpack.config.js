744bc3ebbd3d9f91d8f0e60a6412f1943f58de9e
chenxianggit <328692993@qq.com>
2017-08-01 15:34:10 +0800

    配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,5 @@ const path = require('path');
 
 let env = process.env.NODE_ENV;
 
-module.exports = require(path.resolve(__dirname, 'cfg', env));
+//module.exports = require(path.resolve(__dirname, 'cfg', env));  //原配置，报错
+module.exports = require(path.resolve(__dirname, 'cfg/dev.js'));
